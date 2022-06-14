928725fb68096241c8c406508d3ef5287824df1f
JakeAwsd <93892085+JakeAwsd@users.noreply.github.com>
2022-06-13 23:32:10 -0500

    finished adding routes and figured out what to do with the db

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
-const express = require('express');
-const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const express = require('express');
+const apiRoutes = require('./routes/noteRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+// Initialize the app and create a port
 const app = express();
-
-// Import custom middleware, "cLog"
 app.use(clog);
 
-// Middleware for parsing JSON and urlencoded form data
+const PORT = process.env.PORT || 3001;
+
+// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: 🚀 ${PORT} 🚀`));
